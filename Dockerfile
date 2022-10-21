d450c513415d69e3485c1b3e55f4fee4e0355b57
Jaesin Mulenex <jason.mulenex@cbsinteractive.com>
2022-10-21 12:17:37 -0700

    Customize the phar builder for php 8.1 compatibility.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,31 @@
-FROM vimeo/psalm-github-actions:latest
+FROM php:8.1-alpine
 
-## Build a phar for psalm ##
-RUN apk --no-cache add bash \
-  && git clone https://github.com/vimeo/psalm.git /app/psalm \
-  && cd /app/psalm \
-  && git checkout 4.9.2 \
-  && composer install \
-  && bin/build-phar.sh \
-  && cp build/psalm.phar /usr/local/bin/psalm \
-  && /usr/local/bin/psalm --version
+## Attempt 1 ##
+ENV COMPOSER_ALLOW_SUPERUSER=1
+
+COPY --from=composer:2.4 /usr/bin/composer /usr/bin/composer
+
+RUN apk --no-cache add bash git \
+  && git clone https://github.com/vimeo/psalm.git /app/psalm
+
+WORKDIR /app/psalm
+
+RUN git checkout 4.29.0 \
+    && composer install --no-progress \
+    && composer bin box require --no-progress humbug/box:'^3.16.0'
+
+COPY src/scoper.inc.php /app/psalm/scoper.inc.php
+
+RUN bin/build-phar.sh \
+    && cp build/psalm.phar /usr/local/bin/psalm \
+    && chmod 755 /usr/local/bin/psalm \
+    && ls -hal /usr/local/bin/psalm
+
+## Used for debugging the build ##
+#RUN psalm --version
+#
+#RUN php -r '$phar = new Phar("build/psalm.phar"); $phar->extractTo("psalm-phar-extracted");'
+#RUN grep -r PSALM_VERSION psalm-phar-extracted
+#RUN grep -r PHP_PARSER_VERSION psalm-phar-extracted
+
+ENTRYPOINT /usr/local/bin/psalm
---
+++ src/scoper.inc.php
@@ -0,0 +1,101 @@
+<?php
+
+use Composer\Autoload\ClassLoader;
+
+return [
+  'patchers' => [
+    function ($filePath, $prefix, $contents) {
+      //
+      // PHP-Parser patch
+      //
+      if ($filePath === 'vendor/nikic/php-parser/lib/PhpParser/NodeAbstract.php') {
+        $length = 15 + strlen($prefix) + 1;
+
+        return preg_replace(
+          '%strpos\((.+?)\) \+ 15%',
+          sprintf('strpos($1) + %d', $length),
+          $contents
+        );
+      }
+
+      return $contents;
+    },
+    function ($filePath, $prefix, $contents) {
+      return str_replace(
+        '\\'.$prefix.'\Composer\Autoload\ClassLoader',
+        '\Composer\Autoload\ClassLoader',
+        $contents
+      );
+    },
+    function ($filePath, $prefix, $contents) {
+      if (strpos($filePath, 'src/Psalm') === 0) {
+        return str_replace(
+          [' \\PhpParser\\'],
+          [' \\' . $prefix . '\\PhpParser\\'],
+          $contents
+        );
+      }
+
+      return $contents;
+    },
+    function ($filePath, $prefix, $contents) {
+      if (strpos($filePath, 'vendor/phpmyadmin/sql-parser/src/Context.php') === 0) {
+        return str_replace(
+          '\'' . $prefix,
+          '\'\\\\' . $prefix,
+          $contents
+        );
+      }
+
+      return $contents;
+    },
+    function ($filePath, $prefix, $contents) {
+      if (strpos($filePath, 'vendor/openlss') === 0) {
+        return str_replace(
+          $prefix . '\\DomDocument',
+          'DomDocument',
+          $contents
+        );
+      }
+
+      return $contents;
+    },
+    function ($filePath, $prefix, $contents) {
+      if ($filePath === 'src/Psalm/Internal/Cli/Psalm.php') {
+        return str_replace(
+          '\\' . $prefix . '\\PSALM_VERSION',
+          'PSALM_VERSION',
+          $contents
+        );
+
+      }
+
+      return $contents;
+    },
+    function ($filePath, $prefix, $contents) {
+      $ret = str_replace(
+        $prefix . '\\Psalm\\',
+        'Psalm\\',
+        $contents
+      );
+      return $ret;
+    },
+  ],
+  'whitelist' => [
+    ClassLoader::class,
+    Stringable::class,
+    'Psalm\*',
+  ],
+  'exclude-files' => [
+    'src/spl_object_id.php',
+    'vendor/symfony/polyfill-php80/Php80.php',
+    'vendor/symfony/polyfill-php80/PhpToken.php',
+    'vendor/symfony/polyfill-php80/Resources/stubs/Attribute.php',
+    'vendor/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
+    'vendor/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+  ],
+  'expose-constants' => [
+    'PSALM_VERSION',
+    'PHP_PARSER_VERSION',
+  ],
+];
